555524a2b3beb6dad4b48fb4dc2acc25a5a60e44
Simratpreet <sim.singh6@gmail.com>
2014-08-09 14:31:05 +0530

    Login System dev design left

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+validates :email, :first_name, presence: true
+validates :email, uniqueness: {case_sensitive: false}
+validates :email, format: { with: /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})\z/, :message => "Only emails allowed" }
+
+
+
+end
