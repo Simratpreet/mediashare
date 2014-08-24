cdadac9d840c650c5f8605bb4f7ac0339dd5f539
Aditya Kapoor <aditya.kapoor@vinsol.com>
2014-08-24 16:17:30 +0530

    remove ignored files

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def render_media_object(media)
+    if media.image?
+      image_tag media.attachment.url(:thumb)
+    elsif media.video?
+      video_tag media.attachment, controls: true, autobuffer: true, class: 'video'
+    else
+      link_to "Media Name", media.attachment.url
+    end
+  end
 end
